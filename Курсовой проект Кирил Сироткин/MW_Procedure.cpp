f020170c3d518522ff7dc3af895293ae9ccd8369
SinglePlayer-01 <157644774+SinglePlayer-01@users.noreply.github.com>
2024-06-03 13:08:53 +0300

    Create ping ponk

--- Курсовой проект Кирил Сироткин/MW_Procedure.cpp
+++ Курсовой проект Кирил Сироткин/MW_Procedure.cpp
@@ -1,10 +1,135 @@
-#include "MainWindow.h"
+﻿#include "MainWindow.h"
+#include <stdio.h> 
 
+// Собитійно-орієнтовна функція для створення моделі поведірки ввікна
 LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	switch (uMsg)
-	{
-		
-	}
-	return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
+    switch (uMsg)
+    {
+        // Реагування на зміну розміра вікна
+    case WM_SIZE:
+        GetClientRect(m_hwnd, &clientRect);
+        rc_platform.left = platform_x_1;
+        rc_platform.top = clientRect.bottom - platform_y_1;
+        rc_platform.right = platform_x_2;
+        rc_platform.bottom = clientRect.bottom;
+        return 0;
+
+
+        // Переміщення платформи при реагуванні на кнопки <- ->
+    case WM_KEYDOWN:
+    {
+        switch (wParam)
+        {
+        case VK_LEFT:
+        {
+            if (platform_x_1 != 0)
+            {
+                platform_x_1 -= 30;
+                platform_x_2 -= 30;
+                rc_platform.left = platform_x_1;
+                rc_platform.top = clientRect.bottom - platform_y_1;
+                rc_platform.right = platform_x_2;
+                rc_platform.bottom = clientRect.bottom;
+                InvalidateRect(m_hwnd, NULL, TRUE);
+            }
+        }
+        break;
+        case VK_RIGHT:
+        {
+            GetClientRect(m_hwnd, &clientRect);
+            if (platform_x_2 != clientRect.right)
+            {
+                platform_x_1 += 30;
+                platform_x_2 += 30;
+                rc_platform.left = platform_x_1;
+                rc_platform.top = clientRect.bottom - platform_y_1;
+                rc_platform.right = platform_x_2;
+                rc_platform.bottom = clientRect.bottom;
+                InvalidateRect(m_hwnd, NULL, TRUE);
+            }
+        }
+        break;
+        }
+        return 0;
+    }
+
+    case WM_CREATE:
+    {
+        SetTimer(m_hwnd, 1, 16, NULL); // Таймер для обновления анимации
+
+        // Creating the font we need
+
+        HFONT hFont = CreateFont(50, 0, 0, 0, FW_BOLD, 0, 0, 0, 0, 0, 0, 0, 0, L"Arial");
+
+
+        hwnd_edit = CreateWindow(L"EDIT", L"0", WS_CHILD | ES_CENTER | ES_READONLY | WS_VISIBLE, 0, 0, 100, 60, m_hwnd, NULL, NULL, NULL, NULL);
+        // Set your font
+        SendMessage(hwnd_edit, WM_SETFONT, (WPARAM)hFont, TRUE);
+
+        GetClientRect(m_hwnd, &clientRect);
+    }
+    break;
+    case WM_TIMER:
+        // Обновление координат шарика
+        ballX += ballSpeedX;
+        ballY += ballSpeedY;
+
+        // Проверка столкновения с границами окна
+        GetClientRect(m_hwnd, &clientRect);
+        if (ballX - ballRadius < 0 || ballX + ballRadius > clientRect.right)
+        {
+            ballSpeedX = -ballSpeedX;
+        }
+        if (ballY - ballRadius < 0 || ballY + ballRadius > clientRect.bottom)
+        {
+            ballSpeedY = -ballSpeedY;
+        }
+
+        if (rc_platform.left <= ballX && ballX <= rc_platform.right && ballY + ballRadius > clientRect.bottom - 20)
+        {
+            score++;
+            ballSpeedY = -ballSpeedY;
+        }
+
+
+        // Перерисовка окна
+        InvalidateRect(m_hwnd, NULL, TRUE);
+        break;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(m_hwnd, &ps);
+
+
+        // Преобразование int to wchar_t
+        wchar_t m_reportFileName[100];
+        swprintf_s(m_reportFileName, L"%d", score);
+
+        // Рисование шарика
+        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 5));
+        FillRect(hdc, &rc_platform, (HBRUSH)(COLOR_WINDOW + 9));
+        Ellipse(hdc, ballX - ballRadius, ballY - ballRadius, ballX + ballRadius, ballY + ballRadius);
+        SetWindowText(hwnd_edit, m_reportFileName);
+
+        EndPaint(m_hwnd, &ps);
+        break;
+    }
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+
+    case WM_CTLCOLORBTN:
+    case WM_CTLCOLOREDIT:
+    case WM_CTLCOLORSTATIC:
+        SetTextColor(HDC(wParam), (COLORREF)RGB(0, 255, 0));
+        SetBkMode(HDC(wParam), (COLORREF)RGB(200, 0, 0));
+        return LRESULT(HBRUSH(GetStockObject(NULL_BRUSH)));
+
+
+    default:
+        return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
+    }
+    return 0;
 }
--- Курсовой проект Кирил Сироткин/MainWindow.h
+++ Курсовой проект Кирил Сироткин/MainWindow.h
@@ -1,10 +1,32 @@
-#pragma once
-
-#include<Windows.h>
+﻿#include<Windows.h>
 #include"BaseWindow.h"
 
+// Основні властивості вікна
 class MainWindow : public BaseWindow<MainWindow>
 {
+private:
+	// розміри вікна
+	RECT clientRect;
+	// Параметри кулі
+	int ballRadius = 20;
+	int ballX = 100;
+	int ballY = 100;
+	int ballSpeedX = 5;
+	int ballSpeedY = 5;
+
+	// Параметри платформи
+	int platform_x_1 = 0;
+	int platform_x_2 = 100;
+	int platform_y_1 = 20;
+	int platform_y_2 = 0;
+	RECT rc_platform;
+
+	// Создание счётчика 
+
+	int score = 0;
+	wchar_t m_reportFileName[100];
+	HFONT hFont;
+	HWND hwnd_edit;
 
 public:
 	PCWSTR ClassName() const { return L"MainWindow class"; }
